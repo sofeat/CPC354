6d4b0b7dff23b9a41c63acefe351c984446f366f
Huda <104578385+hudnabilah@users.noreply.github.com>
2023-12-04 03:06:27 +0800

    Update to 3d Triangle

--- Triangle.js
+++ Triangle.js
@@ -1,6 +1,7 @@
 var gl;
 var points = [];
 var NumTimesToSubDivide = 5;
+var colors = [];
 
 function initWebGL() {
     var canvas = document.getElementById("game-surface");
@@ -12,47 +13,93 @@ function initWebGL() {
     }
 
     var vertices = [
-        vec2(-1.0, -1.0),
-        vec2(0.0, 1.0),
-        vec2(1.0, -1.0)
+        vec3(0.0000, 0.0000, -1.0000),
+        vec3(0.0000, 0.9428, 0.3333),
+        vec3(-0.8165, -0.4714, 0.3333),
+        vec3(0.8165, -0.4714, 0.3333)
     ];
 
-    divideTriangle(vertices[0], vertices[1], vertices[2], NumTimesToSubDivide);
+    divideTetra(vertices[0], vertices[1], vertices[2], vertices[3], NumTimesToSubDivide);
+
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    // enable hidden-surface removal
+
+    gl.enable(gl.DEPTH_TEST);
 
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
+
+    // Create a buffer object, initialize it, and associate it with the
+    //  associated attribute variable in our vertex shader
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
 
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
     var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    var uColor = gl.getUniformLocation(program, "uColor");
-    gl.uniform4fv(uColor, vec4(1.0, 0.0, 0.0, 1.0)); // Red color
 
     render();
 }
 
-function triangle(a, b, c) {
-    points.push(a, b, c);
+function triangle(a, b, c, color) {
+
+    // add colors and vertices for one triangle
+
+    var baseColors = [
+        vec3(1.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        vec3(0.0, 0.0, 1.0),
+        vec3(0.0, 0.0, 0.0)
+    ];
+
+    colors.push(baseColors[color]);
+    points.push(a);
+    colors.push(baseColors[color]);
+    points.push(b);
+    colors.push(baseColors[color]);
+    points.push(c);
 }
 
-function divideTriangle(a, b, c, count) {
+function tetra(a, b, c, d) {
+    // tetrahedron with each side using
+    // a different color
+
+    triangle(a, c, b, 0);
+    triangle(a, c, d, 1);
+    triangle(a, b, d, 2);
+    triangle(b, c, d, 3);
+}
+
+function divideTetra(a, b, c, d, count) {
     if (count === 0) {
-        triangle(a, b, c);
+        tetra(a, b, c, d);
     } else {
         var ab = mix(a, b, 0.5);
         var ac = mix(a, c, 0.5);
+        var ad = mix(a, d, 0.5);
         var bc = mix(b, c, 0.5);
+        var bd = mix(b, d, 0.5);
+        var cd = mix(c, d, 0.5);
 
         --count;
 
-        divideTriangle(a, ab, ac, count);
-        divideTriangle(c, ac, bc, count);
-        divideTriangle(b, bc, ab, count);
+        divideTetra(a, ab, ac, ad, count);
+        divideTetra(ab, b, bc, bd, count);
+        divideTetra(ac, bc, c, cd, count);
+        divideTetra(ad, bd, cd, d, count);
     }
 }
 
